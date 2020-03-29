b29d0ea7143278dc7c61acd2641988f17293d45c
cod3breaker <ravisherath@gmail.com>
2020-03-29 23:10:11 +1100

    adding NoteDetailVC

---
+++ LockedNotes/Controller/NoteDetailVC.swift
@@ -0,0 +1,29 @@
+//
+//  NoteDetailVC.swift
+//  LockedNotes
+//
+//  Created by Sunimal Herath on 29/3/20.
+//  Copyright © 2020 Sunimal Herath. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailVC: UIViewController {
+
+    // IBOutlets
+    @IBOutlet weak var noteTextView: UITextView!
+    
+    // Variables
+    var note: Note! // passing note from the tableview
+    var index: Int! // to indentify the note been locked
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // IBActions
+    @IBAction func lockNoteBtnWasPressed(_ sender: Any) {
+        // lock note
+    }
+}
