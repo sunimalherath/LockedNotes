1debbcf169a905d56ce585b2ad1461f25c51b17c
cod3breaker <ravisherath@gmail.com>
2020-03-29 19:06:58 +1100

    adding table view methods to NotesVC

--- LockedNotes/Controller/NotesVC.swift
+++ LockedNotes/Controller/NotesVC.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class NotesVC: UIViewController {
+    
+    //IBOutlets
+    @IBOutlet weak var tableView: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+}
 
-
+extension NotesVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
